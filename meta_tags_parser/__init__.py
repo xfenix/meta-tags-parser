4a7a4da3ee63b48a52ff8d1d354c1f903a82d5a3
Denis <ad@xfenix.ru>
2025-08-20 02:12:08 +0300

    refactor: reintroduce __all__

--- meta_tags_parser/__init__.py
+++ meta_tags_parser/__init__.py
@@ -1,12 +1,21 @@
-from . import parse as parse
-from . import public as public
-from . import snippets as snippets
-from . import structs as structs
+from typing import Final
+
+from .parse import parse_meta_tags_from_source
+from .public import (
+    parse_snippets_from_url,
+    parse_snippets_from_url_async,
+    parse_tags_from_url,
+    parse_tags_from_url_async,
+)
+from .snippets import parse_snippets_from_source
 
 
-parse_meta_tags_from_source = parse.parse_meta_tags_from_source
-parse_snippets_from_source = snippets.parse_snippets_from_source
-parse_snippets_from_url = public.parse_snippets_from_url
-parse_snippets_from_url_async = public.parse_snippets_from_url_async
-parse_tags_from_url = public.parse_tags_from_url
-parse_tags_from_url_async = public.parse_tags_from_url_async
+__all__: Final[tuple[str, ...]] = (
+    "parse_meta_tags_from_source",
+    "parse_snippets_from_source",
+    "parse_snippets_from_url",
+    "parse_snippets_from_url_async",
+    "parse_tags_from_url",
+    "parse_tags_from_url_async",
+)
+
