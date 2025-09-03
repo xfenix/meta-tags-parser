e0e1bf91b20a385618a68f51b61b222ac5731dbf
Denis <ad@xfenix.ru>
2025-09-04 02:42:25 +0300

    Use ContextVar for global options

--- meta_tags_parser/download.py
+++ meta_tags_parser/download.py
@@ -1,11 +1,14 @@
+import typing
+
 import httpx
 
 
 def download_page_sync(uri_of_page: str) -> str:
-    return httpx.get(uri_of_page).text
+    response_obj: typing.Final[httpx.Response] = httpx.get(uri_of_page)
+    return response_obj.text
 
 
 async def download_page_async(uri_of_page: str) -> str:
     async with httpx.AsyncClient() as client:
-        request_obj: typing.Final[httpx.Response] = await client.get(uri_of_page)
-        return request_obj.text
+        response_obj: typing.Final[httpx.Response] = await client.get(uri_of_page)
+        return response_obj.text
